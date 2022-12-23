ab64df2ba8938fd3d3c1b0b7e640cf21da54ff23
Carlos Villadiego <carlovinar93@gmail.com>
2022-12-22 22:15:37 -0500

    se agrego paginación

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
+const botonSiguiente = document.getElementById('btnSiguiente');
+const botonAnterior = document.getElementById('btnAnterior');
+let pagina = 1;
+
+botonSiguiente.addEventListener('click', ()=>{
+    pagina += 1;
+    cargarPeliculas();
+
+
+});
+
+
+botonAnterior.addEventListener('click', ()=>{
+    if(pagina <= 36402){
+
+        pagina -= 1;
+        cargarPeliculas();
+        
+    }
+
+    
+
+
+});
+
+
+
 const cargarPeliculas = async()=>{
     //se coencta a pa API para cargar las peliculas
     //se le paso  el idioma español como parametro en la URL de la API
   try {
-    const respuesta =  await fetch('https://api.themoviedb.org/3/movie/popular?api_key=fcf243062599b41c3af1ed0d55e774e4&language=es-ES');
+    const respuesta =  await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=fcf243062599b41c3af1ed0d55e774e4&language=es-ES&page=${pagina}`);
     console.log(respuesta);
     // convertir la respuesta a JSON para poder trabajarla.
     // COMPROBAR SI LA RESPUESTA ES CORRECTA
@@ -13,7 +40,13 @@ const cargarPeliculas = async()=>{
 
         let peliculas = '';
         data.results.forEach(pelicula => {
-         peliculas = peliculas + `<h1>${pelicula.title}</h1>`;
+         peliculas += `
+         <div class="pelicula">
+         <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+         <h3 class="titulo">${pelicula.title}</h3>
+         <p>Estreno: ${pelicula.release_date}</p>
+         <p class="promedio">Puntuación: ${pelicula.vote_average}</p>
+         </div>`;
             
         });
 
